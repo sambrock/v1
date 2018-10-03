1461409aa9698efb66a3e0aa9520d43a22f3dbc7
sambrock <sambrocklehurst1998@gmail.com>
2018-10-03 19:51:30 +0100

    Changed mouseout event to mouseleave.

--- js/js.js
+++ js/js.js
@@ -1,18 +1,11 @@
-//$(".work-img").hover(function(){
-//    var info = $(".work-info");
-//    info.fadeToggle(300);
-//})
-
 $(".work-col").mouseenter(function(e){
     var info = $(this).children().next();
     info.fadeIn(300);
     e.preventDefault;
-    console.log(info);
 })
 
-$(".work-info").mouseout(function(e){
+$(".work-info").mouseleave(function(e){
     var info = $(".work-info");
     info.fadeOut(300);
     e.preventDefault;
 })
-
