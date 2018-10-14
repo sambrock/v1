92cf6c17675ddd66a99773492bb04b0354727fff
sambrock <sambrocklehurst1998@gmail.com>
2018-10-14 20:41:53 +0100

    Commented code. Added IE support.

--- js/js.js
+++ js/js.js
@@ -1,3 +1,6 @@
+//JS document -
+//Created by Sam Brocklehurst
+
 $(".work-col").mouseenter(function(e){
     var info = $(this).children().next();
     info.fadeIn(300);
@@ -10,10 +13,18 @@ $(".work-info-container").mouseleave(function(e){
     e.preventDefault;
 })
 
+//Resize parent element depending on absolute positined child element
 function resize(){
     var parent = $("#my-skills")
     var child = $("#skills-container").height();
     parent.css("height", child-90);
+}
+
+//If user's browser is Microsoft Edge add a 10px margin around the work cols since grid-gap is not supported
+function ifEdge(){
+    if (/Edge\/\d./i.test(navigator.userAgent)){
+        $(".work-col").css("margin", "10px");
+    }
 }
 
 $(window).on('resize', function(e) {
@@ -22,4 +33,6 @@ $(window).on('resize', function(e) {
 
 $( document ).ready(function(){
     resize();
+    ifEdge();
 })
+
